fd1b08f8572e35fa688bcfd1305e50721b8069d6
timmyjinks <tysonjjenkins.github@gmail.com>
2025-05-16 15:13:40 -0600

    Add Card component

--- tc/src/lib/components/Card.svelte
+++ tc/src/lib/components/Card.svelte
@@ -2,7 +2,7 @@
 	let { status, name } = $props();
 </script>
 
-<div class="m-[15px] w-[25%] rounded-lg bg-[#272727] p-[10px]">
+<div class="m-[15px] rounded-lg bg-[#272727] p-[10px]">
 	<h2 class="text-[18px]">{name}</h2>
 	<br />
 	<p>Status: {status}</p>
